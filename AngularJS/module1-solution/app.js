0bba911c5f321f53a83dd62edc1808b6a217c132
Konrad Gumienny <konrad.gumienny@gmail.com>
2020-08-17 18:38:43 +0200

    module 5 solution

--- AngularJS/module1-solution/app.js
+++ AngularJS/module1-solution/app.js
@@ -5,9 +5,11 @@ angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
 
 function LunchCheckController($scope,$filter, $injector){
+	$scope.items = "";
     $scope.sayMessage = function(){
         var words = $scope.items.split(',');
-        if(words.length < 4) $scope.message =  "Enjoy!";
+        if($scope.items == 0) $scope.message =  "Enter data!";
+        else if(words.length < 4) $scope.message =  "Enjoy!";
         else $scope.message = "Too much!";
     }
 }
